a37f25d3b22261d5dd55da58d28678e3efefc69a
notdan <51867420+dann0001@users.noreply.github.com>
2021-07-24 19:57:38 -0400

    Update combat.lua

--- lua-related/combat.lua
+++ lua-related/combat.lua
@@ -1,15 +1,37 @@
--- Airdrop pickup script
+--[[
 
--- Prevent multi loading
-if shared.active then
-    return 
+    Airdrop Script V.2
+        * Fix map changing, which causes script to break
+        * Make the code cleaner and more customizable
+        * Redo player grab notification
+
+]]--
+
+--[[
+    
+ Main Loadstring
+
+shared.distance = 20    -- Distance between player and crate (Most likely useless to change.)
+shared.airdrop  = 20    -- Airdrop notification time
+shared.player   = 180   -- Player notification time
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/dann0001/public-stuff/main/lua-related/combat.lua"),true))()
+
+]]--
+
+-- Loader
+shared.debug = false 
+
+if shared.active and not shared.debug then
+    return
 end
 
 shared.active = true
-warn("Auto Airdrop Pickup Loaded")
 
+warn("Combat Warriors script loaded. (V.2)")
+
+-- Extra setup stuff 
 local bindable = Instance.new("BindableFunction")
-local distance = 20
 
 function bindable.OnInvoke(answer)
     if answer == "Yes" then
@@ -25,44 +47,45 @@ function bindable.OnInvoke(answer)
     end
 end
 
-game:GetService("Workspace").Map:GetChildren()[1].ChildAdded:Connect(function(object)
-    game:GetService("RunService").Heartbeat:wait() -- Avoid problems, delay with spawning or some shit.
-     
-    game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Airdrop Spawned!";
-        Text = "Want to teleport towards it?";
-        Duration = 20;
-        Callback = bindable;
-        Button1 = "Yes";
-        Button2 = "No";
-    })
+workspace.Map.ChildAdded:Connect(function(object)
+    object.ChildAdded:Connect(function(airdrop)  
+        game:GetService("RunService").Heartbeat:wait()
 
-    if object.Name == "Airdrop" then
-        object.ChildRemoved:Connect(function(weld)
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Airdrop Spawned!";
+            Text = "Want to teleport towards it?";
+            Duration = shared.airdrop;
+            Callback = bindable;
+            Button1 = "Yes";
+            Button2 = "No";
+        })
+
+        -- Really ugly code.
+        airdrop.ChildRemoved:Connect(function(weld)
             if weld.Name == "TopWeld" then
                 
                 for _, player in next, game:GetService("Players"):GetChildren() do
-                    local check = (object.Crate.Hitbox.Position - player.Character.HumanoidRootPart.Position).magnitude
-                    wait(0.05) -- Just to make sure..
-                    if check < distance and player.Name ~= game:GetService("Players").LocalPlayer.Name then
-                           local found = player.Backpack:FindFirstChild("RPG-7")
-                           if found then
+                    local check = (airdrop.Crate.Hitbox.Position - player.Character.HumanoidRootPart.Position).magnitude
+                    if check < shared.distance then
+                        wait()
+                        local found = player.Backpack:FindFirstChild("RPG-7")
+                        if found then
 
-                            game:GetService("StarterGui"):SetCore("SendNotification", {
-				Title = "Player obtained crate!";
-				Text = player.Name.."("..player.DisplayName..")";
-				Icon = "rbxthumb://type=AvatarHeadShot&id="..player.UserId.."&w=420&h=420";
-				Duration = 180;
-				Button1 = "Dismiss";
-			     })  
+                        game:GetService("StarterGui"):SetCore("SendNotification", {
+                            Title = "Player obtained crate!";
+                            Text = player.Name.."("..player.DisplayName..")";
+                            Icon = "rbxthumb://type=AvatarHeadShot&id="..player.UserId.."&w=420&h=420";
+                            Duration = shared.player;
+                            Button1 = "Dismiss";
+                        })   
 
                         end
-                    end 
-
+                    end
                 end
 
             end
         end)
-    end
 
+    end)
 end)
+
