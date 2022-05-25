1bed6c7e08511a332944afa4659487a1d1943b9c
notdan <51867420+dann0001@users.noreply.github.com>
2022-05-25 17:11:39 -0400

    Update basic-esp.lua

--- lua-related/basic-esp.lua
+++ lua-related/basic-esp.lua
@@ -1,61 +1,49 @@
---[[
-    BASIC ESP (USES BoxHandleAndornment)
+-- Better ESP using Highlight
+-- Still lazy to add features.
+if game.CoreGui:FindFirstChild("ESPContainer") then return end
 
-    This was written as just some practice.
-    I honestly got bored at 3 AM.
+local Players = game:GetService("Players")
+local Settings = {
 
-    Future Features:
-    - Toggle ESP
-        - This is pretty easy to add, just by adding the ESP Instances to a table and setting visibility
-    - Data Tags
-        - Health displayer
-        - Distance displayer
+}
 
-    Will I add these future features? Probably not but you have an idea on how to do so.
---]]
+local Container = Instance.new("Folder")
+Container.Name = "ESPContainer"
+Container.Parent = game.CoreGui
 
-local PlayersService = game:GetService("Players")
-
---[[
-    PlayerSetup: function()
-    Arguments: Player <Player> (Instance)
-    Description: Apply ESP correctly based off player's current health state. 
---]]
+local function CreateHightlight(Object)
+    local Highlight = (Container:FindFirstChild(Object.Name) or Instance.new("Highlight")) 
+    Highlight.Name = Object.Name
+    Highlight.Adornee = Object
+    Highlight.Parent = Container 
+end
 
 local function PlayerSetup(Player) 
-    -- Make sure our LocalPlayer doesn't get ESP.
-    if Player == PlayersService.LocalPlayer then return end
-    -- Load first time
-    if Player.Character and Player.Character:FindFirstChild("HumanoidRootPart") then
-       local ESP = Instance.new("BoxHandleAdornment")
-       ESP.AlwaysOnTop = true
-       ESP.ZIndex = 1 -- Has to be 1 to be shown above other parts.
-       ESP.Color3 = Color3.fromRGB(0, 0, 255)
-       ESP.Transparency = 0.5
-       ESP.Size = Player.Character.HumanoidRootPart.Size
-       ESP.Adornee = Player.Character.HumanoidRootPart
-       ESP.Parent = Player.Character.HumanoidRootPart 
+
+    if not Player then
+        return
     end
-    -- Load off event when player respawns.
+
+    if Player.Character then
+        CreateHightlight(Player.Character)
+    end
+
     Player.CharacterAdded:Connect(function(Character)
-        if Character:WaitForChild("HumanoidRootPart") then
-           local ESP = Instance.new("BoxHandleAdornment")
-           ESP.AlwaysOnTop = true
-           ESP.ZIndex = 1 -- Has to be 1 to be shown above other parts.
-           ESP.Color3 = Color3.fromRGB(0, 0, 255)
-           ESP.Transparency = 0.5
-           ESP.Size = Character.HumanoidRootPart.Size
-           ESP.Adornee = Character.HumanoidRootPart
-           ESP.Parent = Character.HumanoidRootPart
-        end
+        CreateHightlight(Character)
     end)
+
 end
 
--- Loop through all players and call PlayerSetup on each Player. 
-for _, Player in pairs(PlayersService:GetChildren()) do
-    PlayerSetup(Player) -- Feed function (Value -> Player)
-end
--- Listen to when a player is added to add to our container 
-PlayersService.PlayerAdded:Connect(function(Player)
-    PlayerSetup(Player) -- Feed function (Value -> Player)
+Players.PlayerAdded:Connect(function(Player)
+    PlayerSetup(Player)
 end)
+
+Players.PlayerRemoving:Connect(function(Player)
+    Container:FindFirstChild(Player.Name):Remove()
+end)
+
+for _, Player in pairs(Players:GetChildren()) do
+    if Player ~= Players.LocalPlayer then
+        PlayerSetup(Player)
+    end
+end
